ce75c68cf3435c2af944555cfb36144d78dae5d3
Dominik Hirt <hirt@todo42.de>
2020-05-06 13:47:30 +0200

    task test implementation

--- src/test/java/de/workshops/bookdemo/books/BookControllerTest.java
+++ src/test/java/de/workshops/bookdemo/books/BookControllerTest.java
@@ -1,27 +1,89 @@
 package de.workshops.bookdemo.books;
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONArray;
 
 @SpringBootTest
+@Slf4j
 public class BookControllerTest {
 
     @Autowired
     private BookRestController bookRestController;
+
+    @Autowired
+    private WebApplicationContext context;
     
+    @Autowired
+    private ObjectMapper mapper;
+
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
     
     @Test
     public void testGetAllBooks() throws Exception {
         List<Book> books = bookRestController.getAllBooks();
         assertNotNull(books);
         assertEquals(3, books.size());
+    }
+    
+    @Test
+    public void testGetAllBooksWithREST() throws Exception {
+        
+        // Eclipse Favorites (favorite static members of certain types
+        mockMvc.perform(get(BookRestController.REQUEST_URL))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(3)))
+            .andExpect(jsonPath("$[1].title", is("Clean Code")));
+    }
+
+    @Test
+    public void testGetAllBooksWithRESTResult() throws Exception {
+        
+        MvcResult mvcResult =  mockMvc.perform(get(BookRestController.REQUEST_URL))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String jsonPayload = mvcResult.getResponse().getContentAsString();
+        
+        // pretty print existing (un-pretty) json string 
+        Object json = mapper.readValue(jsonPayload, Object.class);
+        log.debug(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+        
+        Book[] books = mapper.readValue(jsonPayload, Book[].class);
+        assertNotNull(books);
+        assertEquals(3, books.length);
     }
     
     
