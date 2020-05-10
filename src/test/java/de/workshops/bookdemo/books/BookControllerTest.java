4617584dbdd3a1bedbaf580f1aa4ae1572c8ccc0
Dominik Hirt <hirt@todo42.de>
2020-05-11 00:09:27 +0200

    fix compile/merge error

--- src/test/java/de/workshops/bookdemo/books/BookControllerTest.java
+++ src/test/java/de/workshops/bookdemo/books/BookControllerTest.java
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
